0eeae12d4fd253147540677a0bfb9653c7d4cca0
oyeyipo45 <oyeyipo45@gmail.com>
2020-06-05 07:26:27 +0100

    vm

--- DBconnection/connection.js
+++
@@ -1,8 +0,0 @@
-const monogoose = require('mongoose');
-
-const url = 'mongodb+srv://damilola:Kolade11.@cluster0-s7fen.mongodb.net/man?retryWrites=true&w=majority'
-
-
-const connectDB = async () => {
-    monogoose.connect 
-}
