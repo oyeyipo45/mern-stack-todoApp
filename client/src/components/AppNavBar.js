230fa7872d26400fdf0d46aa6d16f17032e64ac1
oyeyipo45 <oyeyipo45@gmail.com>
2020-06-06 02:34:00 +0100

    navBar completed

---
+++ client/src/components/AppNavBar.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react"
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from "reactstrap";
+
+
+ class AppNavBar extends Component {
+    
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen : !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">Todo App</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem >
+                                <NavLink href="https://twitter.com/d_oyeyipo">
+                                    Twitter 
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+
+export default AppNavBar;
+
